8d650eaf2979281bbc518330524471b851d88d1d
seollemi <kc.pingol6104@gmail.com>
2025-04-06 21:34:56 +0800

    Fix and add some assets

--- AREA2D1.gd
+++ AREA2D1.gd
@@ -8,11 +8,10 @@ func _on_body_entered(body: PhysicsBody2D) -> void:
 
 
 func _on_body_exited(body) -> void:
-	entered = false
+	if body.name == "Player":
+		entered = false
 
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene_to_file("res://outsidehouse.tscn"
-			
-			)
+			get_tree().change_scene_to_file("res://outsidehouse.tscn")
